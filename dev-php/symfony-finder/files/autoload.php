50c2eafb031df87147c13ab2f72f1192d46e7a24
Guillaume Seren <guillaumeseren@gmail.com>
2017-02-22 18:17:06 +0100

    dev-php/symfony-finder: Add QA fixes

--- dev-php/symfony-finder/files/autoload.php
+++ dev-php/symfony-finder/files/autoload.php
@@ -1,5 +1,5 @@
 <?php
-/* Autoloader for dev-php/symfony-dependency-injection and its dependencies */
+/* Autoloader for symfony-finder and its dependencies */
 
 if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
     require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
